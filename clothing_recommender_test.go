b88ea96f14a3e13491a6bddb61856750cb3a2dfa
Quintin Leary <qdl@umich.edu>
2024-01-24 16:53:01 -0800

    Load env in test file

--- clothing_recommender_test.go
+++ clothing_recommender_test.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 	"os"
 	"testing"
+
+	"github.com/joho/godotenv"
 )
 
 func TestGetRecommendation(t *testing.T) {
+	if err := godotenv.Load(); err != nil {
+		fmt.Print("No env file found")
+		return
+	}
 	err := GetRecommendation(os.Getenv("LOCATION"), os.Getenv("PHONE_NUMBER"))
 	if err != nil {
 		t.Errorf("Failed to get temperature: %v", err)
